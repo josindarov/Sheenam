264a1414444a80a655fb8c236dfa2d7f1fc01105
Jurabek <jorabeksindarov44@gmail.com>
2023-01-07 11:59:18 +0500

    Add new unit tests

---
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -0,0 +1,16 @@
+using Sheenam.Api.Brokers.Foundation.Guests;
+using Sheenam.Api.Brokers.StorageBroker;
+
+namespace Sheenam.Api.Services.Foundations.Guests;
+
+public class GuestService : IGuestService
+{
+    private readonly IStorageBroker storageBroker;
+
+    public GuestService(IStorageBroker storageBroker) =>
+        this.storageBroker = storageBroker;
+    
+    public ValueTask<Guest> AddGuestAsync(Guest guest)=>
+        this.storageBroker.InsertGuestAsync(guest);
+    
+}
